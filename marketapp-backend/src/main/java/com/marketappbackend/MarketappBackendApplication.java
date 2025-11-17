648588cf6641b8c28d8bce5d1ae17f53502bf289
AbraraliS <abrarali.sunasara28@gmail.com>
2025-11-17 19:35:37 +0530

    spring-boot backend created

---
+++ marketapp-backend/src/main/java/com/marketappbackend/MarketappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marketappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketappBackendApplication.class, args);
+    }
+
+}
---
+++ marketapp-backend/src/test/java/com/marketappbackend/MarketappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
