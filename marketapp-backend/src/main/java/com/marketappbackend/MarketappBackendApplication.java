582a1a13dacfdbfa48c6e8cac29bfbf932e6d1b6
AbraraliS <abrarali.sunasara28@gmail.com>
2025-11-18 20:28:39 +0530

    first commit

--- marketapp-backend/src/main/java/com/marketappbackend/MarketappBackendApplication.java
+++ marketapp-backend/src/main/java/com/marketappbackend/MarketappBackendApplication.java
@@ -3,7 +3,7 @@ package com.marketappbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@com.marketappbackend.SpringBootApplication
+@SpringBootApplication
 public class MarketappBackendApplication {
 
     public static void main(String[] args) {
